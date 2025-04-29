75dcf26ac296562c53bc0aa5499831a396ecd6d7
lianyu <1319764245@qq.com>
2025-04-29 22:49:02 +0800

    init: 新增全局状态管理 pinia

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
 import { healthUsingGet } from '@/api/healthController.ts'
+import { useLoginUserStore } from '@/stores/loginUserStore.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
 
 healthUsingGet().then((res) => {
   console.log('res', res)
