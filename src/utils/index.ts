44dba50d727463bc548c99f84f6490ae12ae8c62
lianyu <lian_cx@163.com>
2025-05-29 22:31:45 +0800

    feat: 格式化文件大小工具类逻辑优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,7 +8,7 @@ import { saveAs } from 'file-saver'
 export const formatSize = (size: number) => {
   if (!size) return '未知'
   if (size < 1024) return size + 'B'
-  if (size > 1024) return (size / 1024).toFixed(2) + 'KB'
+  if (size > 1024 && size < 1024 * 1024) return (size / 1024).toFixed(2) + 'KB'
   return (size / (1024 * 1024)).toFixed(2) + 'MB'
 }
 
