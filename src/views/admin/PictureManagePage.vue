c8699b59902879b0adf02689976338b4c5644ff6
lianyu <lian_cx@163.com>
2025-08-27 17:09:17 +0800

    optimize: 优化在图片管理页创建图片时跳转新页面为在当前页跳转

--- src/views/admin/PictureManagePage.vue
+++ src/views/admin/PictureManagePage.vue
@@ -3,7 +3,7 @@
     <a-flex justify="space-between">
       <h2>图片管理</h2>
       <a-space>
-        <a-button type="primary" href="/addPicture" target="_blank">+ 创建图片</a-button>
+        <a-button type="primary" href="/addPicture" target="_self">+ 创建图片</a-button>
         <a-button type="primary" ghost href="/addPicture/batch" target="_blank"
           >+ 批量创建图片</a-button
         >
